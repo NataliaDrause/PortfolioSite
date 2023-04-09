a16670e21b71ba6ab99aa85f3e9b8bdcd3c16a40
NataliaDrause <nataliadrause@gmail.com>
2023-04-09 17:40:21 +0200

    add sticky menu

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,20 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener('click', function() {
   headerEl.classList.toggle('nav-open');
 });
+
+// STICKY NAVIGATION //
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  if(!ent.isIntersecting) {
+    document.body.classList.add('sticky');
+  }
+  if(ent.isIntersecting) {
+    document.body.classList.remove('sticky');
+  }
+}, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-80px"
+})
+obs.observe(sectionHeroEl);
